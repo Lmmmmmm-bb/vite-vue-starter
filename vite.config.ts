ba102d08e1320004373929ea56246fe67ffb8bfb
_lmmmmmm <lmmmmmm12138@gmail.com>
2023-04-21 00:00:56 +0800

    chore: `path` to `node:path`

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { resolve } from 'path';
 import unocss from 'unocss/vite';
+import { resolve } from 'node:path';
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vitest/config';
 import autoImport from 'unplugin-auto-import/vite';
@@ -8,7 +8,7 @@ import vueComponents from 'unplugin-vue-components/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   // github pages base url
-  // if you using Vercel to deploy, you can remove this line
+  // if you using Vercel to deploy, you can remove next line
   base: '/vite-vue-starter/',
   test: { environment: 'jsdom' },
   plugins: [
